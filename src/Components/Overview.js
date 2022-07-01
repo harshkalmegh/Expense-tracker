f421f29adae41ce52f92dca8de699fc0194421a2
Harsh Kalmegh <87758104+harshkalmegh@users.noreply.github.com>
2022-07-01 17:42:14 +0530

    Update Overview.js

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -5,6 +5,15 @@ import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
+import db from "../Utils/firebase";
+import "devextreme/dist/css/dx.material.orange.dark.css";
+import DataGrid, {
+  SearchPanel,
+  Paging,
+  Column,
+} from "devextreme-react/data-grid";
+import expense from "../Utils/expense";
+import income from "../Utils/income";
 
 import Button from "@mui/material/Button";
 import Datagrid from "./Datagrid";
@@ -17,6 +26,19 @@ const theme = createTheme({
 
 function Overview() {
   const navigate = useNavigate();
+  const [transactionList, setTransactionList] = React.useState([]);
+
+  React.useEffect(() => {
+    const transactionRef = db.database().ref("Transaction");
+    transactionRef.on("value", (snapshot) => {
+      const transactions = snapshot.val();
+      const list = [];
+      for (let id in transactions) {
+        list.push({ id, ...transactions[id] });
+      }
+      setTransactionList(list);
+    });
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -27,7 +49,7 @@ function Overview() {
             my: { xs: 3, md: 3 },
             p: { xs: 1 },
             borderBottom: 5,
-            borderBottomColor: "red",
+            borderBottomColor: "white",
           }}
         >
           <Container
@@ -55,6 +77,74 @@ function Overview() {
             >
               Home
             </Button>
+          </Container>
+        </Paper>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Paper
+          variant="outlined"
+          sx={{
+            my: { xs: 3, md: 4 },
+            p: { xs: 3, md: 4 },
+            borderBottom: 5,
+            borderBottomColor: "red",
+          }}
+        >
+          <Container
+            component="main"
+            maxWidth="350px"
+            sx={{
+              mb: 4,
+            }}
+          >
+            <Typography
+              component="h1"
+              variant="h4"
+              align="center"
+              sx={{ margin: 1 }}
+            >
+              Expense Overview
+            </Typography>
+            <DataGrid dataSource={expense(transactionList)}>
+              <Column dataField="type" alignment="left" visible={true} />
+              <Column dataField="amount" alignment="center" visible={true} />
+              <SearchPanel visible={true} />
+              <Paging defaultPageSize={6} />
+            </DataGrid>
+          </Container>
+        </Paper>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Paper
+          variant="outlined"
+          sx={{
+            my: { xs: 3, md: 4 },
+            p: { xs: 3, md: 4 },
+            borderBottom: 5,
+            borderBottomColor: "green",
+          }}
+        >
+          <Container
+            component="main"
+            maxWidth="350px"
+            sx={{
+              mb: 4,
+            }}
+          >
+            <Typography
+              component="h1"
+              variant="h4"
+              align="center"
+              sx={{ margin: 1 }}
+            >
+              Income Overview
+            </Typography>
+            <DataGrid dataSource={income(transactionList)}>
+              <Column dataField="type" alignment="left" visible={true} />
+              <Column dataField="amount" alignment="center" visible={true} />
+              <SearchPanel visible={true} />
+              <Paging defaultPageSize={6} />
+            </DataGrid>
           </Container>
         </Paper>
       </Box>
